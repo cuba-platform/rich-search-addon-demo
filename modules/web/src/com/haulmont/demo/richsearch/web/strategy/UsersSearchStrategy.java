56351204fe18d585e8aac9c437e3e825a44a0652
zaharchenko <zaharchenko@haulmont.com>
2019-03-03 18:34:36 +0400

    CUBA 7.0 version

--- modules/web/src/com/haulmont/demo/richsearch/web/strategy/UsersSearchStrategy.java
+++ modules/web/src/com/haulmont/demo/richsearch/web/strategy/UsersSearchStrategy.java
@@ -44,6 +44,6 @@ public class UsersSearchStrategy implements SearchStrategy {
 
     @Override
     public String name() {
-        return "usersSearchStrategy";
+        return "searchStrategy.usersSearchStrategy";
     }
 }
